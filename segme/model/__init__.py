e1ad94a843878705338da703e8b6c23da2d52abf
Shkarupa Alex <shkarupa.alex@gmail.com>
2020-11-12 14:26:03 +0400

    U2Net public

--- segme/model/__init__.py
+++ segme/model/__init__.py
@@ -1,3 +1,4 @@
 from .deeplab_v3_plus import build_deeplab_v3_plus
 from .dexi_ned import build_dexi_ned
 from .pyramid_feature_attention import build_pyramid_feature_attention
+from .u2_net import build_u2_net, build_u2_netp
