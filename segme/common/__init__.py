b0abdc9654d88ae5f746734fb5813f010d7b6d75
Shkarupa Alex <shkarupa.alex@gmail.com>
2020-12-10 15:44:44 +0400

    Refactoring

--- segme/common/__init__.py
+++ segme/common/__init__.py
@@ -1,5 +1,5 @@
-from .aspp import ASPP
-from .atsepconv import AtrousSepConv
-from .head import ClassificationHead, RegressionHead
+from .head import HeadProjection, HeadActivation, ClassificationHead
+# from .point_rend import PointRend, point_sample
+from .resizebysample import ResizeBySample, resize_by_sample
+from .resizebyscale import ResizeByScale, resize_by_scale
 from .tochannel import ToChannelFirst, ToChannelLast, to_channel_last, to_channel_first
-from .upbysample import UpBySample, up_by_sample_2d
