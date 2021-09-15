01530b82cf5e579de613696dc0175e7354f4b8cd
Shkarupa Alex <shkarupa.alex@gmail.com>
2021-09-15 21:52:37 +0300

    TriTransNet loss

--- segme/model/__init__.py
+++ segme/model/__init__.py
@@ -5,4 +5,5 @@ from .deeplab_v3_plus import build_deeplab_v3_plus_with_point_rend
 from .dexi_ned import build_dexi_ned
 from .f3_net import build_f3_net, f3net_losses
 from .minet import build_minet, minet_loss
+from .tri_trans import build_tri_trans_net, tri_trans_net_losses
 from .u2_net import build_u2_net, build_u2_netp, u2net_losses
---
+++ segme/model/tri_trans/loss.py
@@ -0,0 +1,5 @@
+from ...loss import PixelPositionAwareLoss
+
+
+def tri_trans_net_losses():
+    return 'SegMe>PixelPositionAwareLoss'
