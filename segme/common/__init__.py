d891bed2e6447fef38d39466fad78b5e21c27556
Shkarupa Alex <shkarupa.alex@gmail.com>
2022-01-26 11:07:42 +0300

    Same-Std-Dw conv layers

--- segme/common/__init__.py
+++ segme/common/__init__.py
@@ -4,7 +4,8 @@ from .featalign import FeatureAlignment
 from .head import HeadProjection, HeadActivation, ClassificationHead
 from .hmsattn import HierarchicalMultiScaleAttention
 from .point_rend import PointRend, PointLoss
-from .stdconv import StandardizedConv2D
+from .sameconv import SameConv, SameStandardizedConv, SameDepthwiseConv, SameStandardizedDepthwiseConv
+from .stdconv import StandardizedConv2D, StandardizedDepthwiseConv2D
 from .resizebysample import ResizeBySample, resize_by_sample
 from .resizebyscale import ResizeByScale, resize_by_scale
 from .tochannel import ToChannelFirst, ToChannelLast, to_channel_last, to_channel_first
--- segme/common/tests/test_stdconv.py
+++ segme/common/tests/test_stdconv.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 from keras import keras_parameterized, testing_utils
 from keras.mixed_precision import policy as mixed_precision
-from ..stdconv import StandardizedConv2D
+from ..stdconv import StandardizedConv2D, StandardizedDepthwiseConv2D
 
 
 @keras_parameterized.run_all_keras_modes
@@ -32,6 +32,38 @@ class TestStandardizedConv2D(keras_parameterized.TestCase):
             input_shape=[2, 16, 16, 8],
             input_dtype='float16',
             expected_output_shape=[None, 8, 8, 4],
+            expected_output_dtype='float16'
+        )
+        self.assertTrue(np.all(np.isfinite(result)))
+
+
+@keras_parameterized.run_all_keras_modes
+class TestStandardizedDepthwiseConv2D(keras_parameterized.TestCase):
+    def setUp(self):
+        super(TestStandardizedDepthwiseConv2D, self).setUp()
+        self.default_policy = mixed_precision.global_policy()
+
+    def tearDown(self):
+        super(TestStandardizedDepthwiseConv2D, self).tearDown()
+        mixed_precision.set_global_policy(self.default_policy)
+
+    def test_layer(self):
+        testing_utils.layer_test(
+            StandardizedDepthwiseConv2D,
+            kwargs={'kernel_size': 1, 'strides': 1, 'padding': 'valid'},
+            input_shape=[2, 16, 16, 8],
+            input_dtype='float32',
+            expected_output_shape=[None, 16, 16, 8],
+            expected_output_dtype='float32'
+        )
+
+        mixed_precision.set_global_policy('mixed_float16')
+        result = testing_utils.layer_test(
+            StandardizedDepthwiseConv2D,
+            kwargs={'kernel_size': 3, 'strides': 2, 'padding': 'same'},
+            input_shape=[2, 16, 16, 8],
+            input_dtype='float16',
+            expected_output_shape=[None, 8, 8, 8],
             expected_output_dtype='float16'
         )
         self.assertTrue(np.all(np.isfinite(result)))
