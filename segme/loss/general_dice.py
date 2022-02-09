db749f25adaf72e8d07926745a18f0594178efda
Shkarupa Alex <shkarupa.alex@gmail.com>
2022-02-09 21:42:48 +0300

    Fix generalized dice

--- segme/loss/general_dice.py
+++ segme/loss/general_dice.py
@@ -18,31 +18,44 @@ class GeneralizedDiceLoss(WeightedLossFunctionWrapper):
 
 def generalized_dice_loss(y_true, y_pred, sample_weight, from_logits):
     y_pred = tf.convert_to_tensor(y_pred)
+    y_true = tf.convert_to_tensor(y_true, 'int32')
+
+    if 4 != y_pred.shape.rank or 4 != y_true.shape.rank:
+        raise ValueError('Both labels and predictions must have rank 4.')
+
+    if y_pred.shape[-1] is None or y_true.shape[-1] is None:
+        raise ValueError('Channel dimension of both labels and predictions must be defined.')
+
+    if 1 != y_true.shape[-1]:
+        raise ValueError('Labels must be sparse-encoded.')
+
+    if from_logits:
+        if 1 == y_pred.shape[-1]:
+            y_pred = tf.nn.sigmoid(y_pred)
+        else:
+            y_pred = tf.nn.softmax(y_pred)
+    if 1 == y_pred.shape[-1]:
+        y_pred = tf.concat([1. - y_pred, y_pred], axis=-1)
+
+    y_true = tf.one_hot(y_true[..., 0], y_pred.shape[-1])
     y_true = tf.cast(y_true, dtype=y_pred.dtype)
 
-    assert_true_rank = tf.assert_rank(y_true, 4)
-    assert_pred_rank = tf.assert_rank(y_pred, 4)
+    epsilon = tf.convert_to_tensor(backend.epsilon(), dtype=y_pred.dtype)
 
-    with tf.control_dependencies([assert_true_rank, assert_pred_rank]):
-        epsilon = tf.convert_to_tensor(backend.epsilon(), dtype=y_pred.dtype)
+    weight = y_true if sample_weight is None else y_true * sample_weight
+    weight = tf.math.divide_no_nan(1., tf.reduce_sum(weight, axis=[1, 2]) ** 2)
+    weight = tf.stop_gradient(weight)
 
-        if from_logits:
-            y_pred = tf.nn.sigmoid(y_pred)
+    intersection = y_pred * y_true
+    intersection = intersection if sample_weight is None else intersection * sample_weight
+    intersection = tf.reduce_sum(intersection, axis=[1, 2]) * weight
+    intersection = tf.reduce_sum(intersection, axis=-1)
 
-        y_pred = tf.concat([y_pred, 1. - y_pred], axis=-1)
-        y_true = tf.concat([y_true, 1. - y_true], axis=-1)
-        y_true = tf.stop_gradient(y_true)
+    union = y_pred + y_true
+    union = union if sample_weight is None else union * sample_weight
+    union = tf.reduce_sum(union, axis=[1, 2]) * weight
+    union = tf.reduce_sum(union, axis=-1)
 
-        weight = y_true if sample_weight is None else y_true * sample_weight
-        weight = 1. / (tf.reduce_sum(weight, axis=[1, 2]) ** 2 + epsilon)
-        weight = tf.stop_gradient(weight)
+    loss = 1. - 2. * intersection / (union + epsilon)
 
-        intersection = y_pred * y_true if sample_weight is None else y_pred * y_true * sample_weight
-        intersection = weight * tf.reduce_sum(intersection, axis=[1, 2])
-        intersection = tf.reduce_sum(intersection, axis=-1)
-        union = y_pred + y_true if sample_weight is None else (y_pred + y_true) * sample_weight
-        union = weight * tf.reduce_sum(union, axis=[1, 2])
-        union = tf.reduce_sum(union, axis=-1)
-        loss = 1. - (2. * intersection + epsilon) / (union + epsilon)
-
-        return loss
+    return loss
--- segme/loss/tests/test_general_dice.py
+++ segme/loss/tests/test_general_dice.py
@@ -9,12 +9,12 @@ from ..general_dice import generalized_dice_loss
 @keras_parameterized.run_all_keras_modes
 class TestGeneralizedDiceLoss(keras_parameterized.TestCase):
     def test_config(self):
-        bce_obj = GeneralizedDiceLoss(
+        loss = GeneralizedDiceLoss(
             reduction=Reduction.NONE,
             name='loss1'
         )
-        self.assertEqual(bce_obj.name, 'loss1')
-        self.assertEqual(bce_obj.reduction, Reduction.NONE)
+        self.assertEqual(loss.name, 'loss1')
+        self.assertEqual(loss.reduction, Reduction.NONE)
 
     def test_zeros(self):
         probs = tf.zeros((1, 16, 16, 1), 'float32')
@@ -23,7 +23,50 @@ class TestGeneralizedDiceLoss(keras_parameterized.TestCase):
         result = generalized_dice_loss(y_true=targets, y_pred=probs, sample_weight=None, from_logits=False)
         result = self.evaluate(result).tolist()
 
-        self.assertAllClose(result, [0.])
+        self.assertAllClose(result, [0.], atol=1e-4)
+
+    def test_ones(self):
+        probs = tf.ones((1, 16, 16, 1), 'float32')
+        targets = tf.ones((1, 16, 16, 1), 'int32')
+
+        result = generalized_dice_loss(y_true=targets, y_pred=probs, sample_weight=None, from_logits=False)
+        result = self.evaluate(result).tolist()
+
+        self.assertAllClose(result, [0.], atol=1e-4)
+
+    def test_false(self):
+        probs = tf.zeros((1, 16, 16, 1), 'float32')
+        targets = tf.ones((1, 16, 16, 1), 'int32')
+
+        result = generalized_dice_loss(y_true=targets, y_pred=probs, sample_weight=None, from_logits=False)
+        result = self.evaluate(result).tolist()
+
+        self.assertAllClose(result, [1.], atol=1e-4)
+
+    def test_true(self):
+        probs = tf.ones((1, 16, 16, 1), 'float32')
+        targets = tf.zeros((1, 16, 16, 1), 'int32')
+
+        result = generalized_dice_loss(y_true=targets, y_pred=probs, sample_weight=None, from_logits=False)
+        result = self.evaluate(result).tolist()
+
+        self.assertAllClose(result, [1.], atol=1e-4)
+
+    def test_multi(self):
+        logits = tf.constant([
+            [[[0.4250706654827763, 7.219920928747051, 1.14131948950217, 2.5576064452206024],
+              [1.342442193620409, 0.20020616879804165, 6.977300484664198, 6.280817910206608]],
+             [[0.3206719246447576, 0.0176225602425912, 1.902292891065069, 3.369106587128292],
+              [2.6576544216404563, 1.863726154333165, 4.581314280496405, 7.433728759092233]],
+             [[8.13888654097292, 1.311411218599392, 0.8372454481780323, 2.859455217953778],
+              [2.0984725413538854, 4.619268334888168, 8.708732477440673, 1.9102341271004541]],
+             [[3.4914178176388266, 4.551627675234152, 7.709902261544302, 3.3982255596983277],
+              [0.9182162683255968, 7.0387004793287886, 2.1883984916630697, 1.3921544038795197]]]], 'float32')
+        targets = tf.constant([[[[1], [3]], [[3], [3]], [[1], [2]], [[2], [1]]]], 'int32')
+
+        loss = GeneralizedDiceLoss(from_logits=True, reduction=Reduction.SUM_OVER_BATCH_SIZE)
+        result = self.evaluate(loss(targets, logits))
+        self.assertAlmostEqual(result, 0.207183837890625, places=6)
 
     def test_value_4d(self):
         logits = tf.constant([
@@ -40,8 +83,8 @@ class TestGeneralizedDiceLoss(keras_parameterized.TestCase):
             [[[0], [1], [1], [0]], [[1], [0], [0], [1]], [[0], [1], [1], [0]], [[1], [1], [1], [1]]]], 'int32')
 
         loss = GeneralizedDiceLoss(from_logits=True, reduction=Reduction.SUM_OVER_BATCH_SIZE)
-        result = self.evaluate(loss(targets, logits)).item()
-        self.assertAlmostEqual(result, 0.5023754239082336, places=7)
+        result = self.evaluate(loss(targets, logits))
+        self.assertAlmostEqual(result, 0.5023754239082336, places=6)
 
     def test_weight_4d(self):
         logits = tf.constant([
@@ -60,18 +103,20 @@ class TestGeneralizedDiceLoss(keras_parameterized.TestCase):
 
         loss = GeneralizedDiceLoss(from_logits=True, reduction=Reduction.SUM_OVER_BATCH_SIZE)
 
-        result = self.evaluate(loss(targets, logits)).item()
-        self.assertAlmostEqual(result, 0.5023754239082336, places=7)
+        result = self.evaluate(loss(targets, logits))
+        self.assertAlmostEqual(result, 0.5023754239082336, places=6)
 
-        result = self.evaluate(loss(targets, logits, weights)).item()
-        self.assertAlmostEqual(result, 0.516532301902771, places=7)
+        result = self.evaluate(loss(targets[:, :, :2, :], logits[:, :, :2, :], weights[:, :, :2, :]))
+        self.assertAlmostEqual(result, 0.5165324211120605, places=7)
+
+        result = self.evaluate(loss(targets, logits, weights))
+        self.assertAlmostEqual(result, 0.5165324211120605, places=7)
 
     def test_keras_model_compile(self):
-        model = models.Sequential([
-            layers.Input(shape=(100,)),
-            layers.Dense(5, activation='sigmoid')]
-        )
-        model.compile(loss='SegMe>generalized_dice_loss')
+        model = models.Sequential([layers.Dense(5, activation='sigmoid')])
+        model.compile(loss='SegMe>GeneralizedDiceLoss')
+        model.fit(np.zeros((2, 16, 16, 1)), np.zeros((2, 16, 16, 1), 'int32'))
+        models.Sequential.from_config(model.get_config())
 
 
 if __name__ == '__main__':
