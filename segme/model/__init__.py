01b8a3043a2203f12bc123ade8c1f6002c764fbb
Shkarupa Alex <shkarupa.alex@gmail.com>
2022-05-27 11:58:04 +0300

    Drop F3net

--- segme/model/__init__.py
+++ segme/model/__init__.py
@@ -3,7 +3,6 @@ from .cascade_psp import build_cascade_psp, cascade_psp_losses, CascadePspRefine
 from .deeplab_v3_plus import build_deeplab_v3_plus, build_deeplab_v3_plus_with_hierarchical_attention
 from .deeplab_v3_plus import build_deeplab_v3_plus_with_point_rend
 from .dexi_ned import build_dexi_ned
-from .f3_net import build_f3_net, f3net_losses
 from .minet import build_minet, minet_loss
 from .tracer import build_tracer, tracer_losses
 from .u2_net import build_u2_net, build_u2_netp, u2net_losses
--- segme/model/f3_net/__init__.py
+++
@@ -1,2 +0,0 @@
-from .model import build_f3_net
-from .loss import f3net_losses
