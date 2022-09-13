3cd98d77f74d46f808ae056eb58b91747c6dabe1
Shkarupa Alex <shkarupa.alex@gmail.com>
2022-09-13 21:34:43 +0300

    Boundary loss refactoring

--- segme/loss/boundary_categorical.py
+++ segme/loss/boundary_categorical.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
-from keras import losses
 from keras.utils.generic_utils import register_keras_serializable
 from keras.utils.losses_utils import ReductionV2 as Reduction
 from tensorflow_addons.image import euclidean_dist_transform
 from segme.loss.common_loss import validate_input, to_probs, to_1hot
+from segme.loss.weighted_wrapper import WeightedLossFunctionWrapper
 
 
 @register_keras_serializable(package='SegMe>Loss')
-class BoundaryCategoricalLoss(losses.LossFunctionWrapper):
+class BoundaryCategoricalLoss(WeightedLossFunctionWrapper):
     """ Proposed in: 'Boundary loss for highly unbalanced segmentation'
 
     Implements Equation (5) from https://arxiv.org/pdf/1812.07032v4.pdf
@@ -17,16 +17,15 @@ class BoundaryCategoricalLoss(losses.LossFunctionWrapper):
         super().__init__(boundary_categorical_loss, reduction=reduction, name=name, from_logits=from_logits)
 
 
-def boundary_categorical_loss(y_true, y_pred, from_logits):
+def boundary_categorical_loss(y_true, y_pred, sample_weight, from_logits):
     y_true, y_pred, sample_weight = validate_input(
-        y_true, y_pred, weight=None, dtype='uint8', rank=4, channel='sparse')
+        y_true, y_pred, sample_weight, dtype='uint8', rank=4, channel='sparse')
     y_pred, from_logits = to_probs(y_pred, from_logits, force_sigmoid=True), False
     y_true, y_pred = to_1hot(y_true, y_pred)
     y_false = 1 - y_true
 
-    axis_hw = list(range(1, y_pred.shape.ndims - 1))
-    has_true = tf.reduce_any(y_true == 1, axis=axis_hw, keepdims=True)
-    has_false = tf.reduce_any(y_true == 0, axis=axis_hw, keepdims=True)
+    has_true = tf.reduce_any(y_true == 1, axis=[1, 2], keepdims=True)
+    has_false = tf.reduce_any(y_true == 0, axis=[1, 2], keepdims=True)
 
     d_true = euclidean_dist_transform(y_true, dtype=y_pred.dtype)
     d_false = euclidean_dist_transform(y_false, dtype=y_pred.dtype)
@@ -36,5 +35,7 @@ def boundary_categorical_loss(y_true, y_pred, from_logits):
     distance = tf.stop_gradient(distance)
 
     loss = y_pred * distance
+    if sample_weight is not None:
+        loss *= sample_weight
 
-    return tf.reduce_mean(loss, axis=-1)
+    return tf.reduce_mean(loss, axis=[1, 2, 3])
