94feb55820b26f1125da6a603f8204b3b4805a82
Shkarupa Alex <shkarupa.alex@gmail.com>
2022-09-13 13:28:08 +0300

    Sobel edge loss refactoring

--- segme/loss/sobel_edge.py
+++ segme/loss/sobel_edge.py
@@ -1,13 +1,14 @@
 import numpy as np
 import tensorflow as tf
-from keras import backend, losses
+from keras import backend
 from keras.utils.generic_utils import register_keras_serializable
 from keras.utils.losses_utils import ReductionV2 as Reduction
 from segme.loss.common_loss import validate_input, to_probs, to_1hot
+from segme.loss.weighted_wrapper import WeightedLossFunctionWrapper
 
 
 @register_keras_serializable(package='SegMe>Loss')
-class SobelEdgeLoss(losses.LossFunctionWrapper):
+class SobelEdgeLoss(WeightedLossFunctionWrapper):
     """ Proposed in: 'CascadePSP: Toward Class-Agnostic and Very High-Resolution
     Segmentation via Global and Local Refinement'
 
@@ -15,11 +16,8 @@ class SobelEdgeLoss(losses.LossFunctionWrapper):
     Compute edge loss with Sobel operator
     """
 
-    def __init__(
-            self, from_logits=False, reduction=Reduction.AUTO,
-            name='sobel_edge_loss'):
-        super().__init__(
-            sobel_edge_loss, reduction=reduction, name=name, from_logits=from_logits)
+    def __init__(self, from_logits=False, reduction=Reduction.AUTO, name='sobel_edge_loss'):
+        super().__init__(sobel_edge_loss, reduction=reduction, name=name, from_logits=from_logits)
 
 
 def sobel(probs):
@@ -32,19 +30,21 @@ def sobel(probs):
     sobel_y = np.tile(sobel_y, [1, 1, classes, 1])
     sobel_y = tf.cast(sobel_y, probs.dtype)
 
-    pooled = tf.nn.avg_pool2d(probs, 3, strides=1, padding='SAME')
-    padded = tf.pad(pooled, [(0, 0), (1, 1), (1, 1), (0, 0)], mode='SYMMETRIC')
-    grad_x = tf.nn.depthwise_conv2d(padded, sobel_x, strides=[1] * 4, padding='VALID')
-    grad_y = tf.nn.depthwise_conv2d(padded, sobel_y, strides=[1] * 4, padding='VALID')
+    pooled = tf.pad(probs, [(0, 0), (1, 1), (1, 1), (0, 0)], mode='SYMMETRIC')
+    pooled = tf.nn.avg_pool2d(pooled, 3, strides=1, padding='VALID')
+
+    grad = tf.pad(pooled, [(0, 0), (1, 1), (1, 1), (0, 0)], mode='SYMMETRIC')
+    grad_x = tf.nn.depthwise_conv2d(grad, sobel_x, strides=[1] * 4, padding='VALID')
+    grad_y = tf.nn.depthwise_conv2d(grad, sobel_y, strides=[1] * 4, padding='VALID')
 
     edge = tf.sqrt(grad_x ** 2 + grad_y ** 2 + backend.epsilon())
 
     return edge
 
 
-def sobel_edge_loss(y_true, y_pred, from_logits):
+def sobel_edge_loss(y_true, y_pred, sample_weight, from_logits):
     y_true, y_pred, sample_weight = validate_input(
-        y_true, y_pred, weight=None, dtype='int32', rank=4, channel='sparse')
+        y_true, y_pred, sample_weight, dtype='int32', rank=4, channel='sparse')
     y_pred, from_logits = to_probs(y_pred, from_logits, force_sigmoid=True), False
     y_true, y_pred = to_1hot(y_true, y_pred)
     y_true = tf.cast(y_true, dtype=y_pred.dtype)
@@ -53,6 +53,9 @@ def sobel_edge_loss(y_true, y_pred, from_logits):
     y_true_edge = tf.stop_gradient(y_true_edge)
     y_pred_edge = sobel(y_pred)
 
-    loss = losses.mean_absolute_error(y_true=y_true_edge, y_pred=y_pred_edge)
+    loss = tf.reduce_mean(tf.abs(y_true_edge - y_pred_edge), axis=-1, keepdims=True)
+    if sample_weight is not None:
+        loss *= sample_weight
+    loss = tf.reduce_mean(loss, axis=[1, 2, 3])
 
     return loss
