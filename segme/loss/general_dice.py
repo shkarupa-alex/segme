c59cd178238d6e60973392c991deb7ca45bc5aa5
Shkarupa Alex <shkarupa.alex@gmail.com>
2022-09-14 00:29:41 +0300

    General DICE loss refactoring

--- segme/loss/general_dice.py
+++ segme/loss/general_dice.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
-from keras import losses
+from keras import backend
 from keras.utils.generic_utils import register_keras_serializable
 from keras.utils.losses_utils import ReductionV2 as Reduction
 from segme.loss.common_loss import validate_input, iou
+from segme.loss.weighted_wrapper import WeightedLossFunctionWrapper
 
 
 @register_keras_serializable(package='SegMe>Loss')
-class GeneralizedDiceLoss(losses.LossFunctionWrapper):
+class GeneralizedDiceLoss(WeightedLossFunctionWrapper):
     """ Proposed in: 'Generalised Dice overlap as a deep learning loss function for highly unbalanced segmentations'
 
     Implements Equations from https://arxiv.org/pdf/1707.03237v3.pdf
@@ -16,15 +17,18 @@ class GeneralizedDiceLoss(losses.LossFunctionWrapper):
         super().__init__(generalized_dice_loss, reduction=reduction, name=name, from_logits=from_logits)
 
 
-def generalized_dice_loss(y_true, y_pred, from_logits):
+def generalized_dice_loss(y_true, y_pred, sample_weight, from_logits):
     y_true, y_pred, sample_weight = validate_input(
-        y_true, y_pred, weight=None, dtype='int32', rank=4, channel='sparse')
+        y_true, y_pred, sample_weight, dtype='int32', rank=4, channel='sparse')
 
     y_true_1h = tf.one_hot(y_true[..., 0], max(2, y_pred.shape[-1]), dtype=y_pred.dtype)
-    weight = (1. if sample_weight is None else sample_weight) / (
-            tf.reduce_sum(y_true_1h, axis=[1, 2], keepdims=True) ** 2 + 1.)
-    weight = tf.stop_gradient(weight)
+    weight = tf.reduce_mean(y_true_1h, axis=[0, 1, 2], keepdims=True) ** 2
+    weight = tf.reduce_max(weight * y_true_1h, axis=-1, keepdims=True) + backend.epsilon()
+    weight = 1. / weight
 
-    loss = iou(y_true, y_pred, weight, from_logits=from_logits, square=False, smooth=1., dice=True)
+    sample_weight = weight if sample_weight is None else sample_weight * weight
+    sample_weight = tf.stop_gradient(sample_weight)
 
-    return tf.reduce_sum(loss, axis=-1)
+    loss = iou(y_true, y_pred, sample_weight, from_logits=from_logits, smooth=1., dice=True)
+
+    return loss
