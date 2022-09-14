Loss tests precision fix for GPU

segme/loss/tests/test_adaptive_intensity.py
@@ -71,13 +71,13 @@ class TestAdaptivePixelIntensityLoss(test_combinations.TestCase):
         loss = AdaptivePixelIntensityLoss(from_logits=True)
 
         result = self.evaluate(loss(targets[:, :, :32], logits[:, :, :32]))
-        self.assertAlmostEqual(result, 2.423418, places=6)
+        self.assertAlmostEqual(result, 2.423418, places=5)
 
         result = self.evaluate(loss(targets, logits, weights))
-        self.assertAlmostEqual(result, 1.1870989, places=6)
+        self.assertAlmostEqual(result, 1.1870989, places=5)
 
         result = self.evaluate(loss(targets, logits, weights * 2.))
-        self.assertAlmostEqual(result, 1.8727206, places=6)
+        self.assertAlmostEqual(result, 1.8727206, places=5)
 
     def test_multi(self):
         logits = tf.tile(MULTI_LOGITS, [1, 16, 16, 1])

segme/loss/tests/test_cross_entropy.py
@@ -61,7 +61,7 @@ class TestCrossEntropyLoss(test_combinations.TestCase):
         loss = CrossEntropyLoss(from_logits=True)
         result = self.evaluate(loss(targets, logits))
 
-        self.assertAlmostEqual(result, 1.5399182)
+        self.assertAlmostEqual(result, 1.5399182, places=6)
 
     def test_weight(self):
         logits = tf.tile(BINARY_LOGITS, [1, 16, 16, 1])
@@ -71,10 +71,10 @@ class TestCrossEntropyLoss(test_combinations.TestCase):
         loss = CrossEntropyLoss(from_logits=True)
 
         result = self.evaluate(loss(targets[:, :, :32], logits[:, :, :32]))
-        self.assertAlmostEqual(result, 1.5399183)
+        self.assertAlmostEqual(result, 1.5399183, places=6)
 
         result = self.evaluate(loss(targets, logits, weights))
-        self.assertAlmostEqual(result, 0.5381311)
+        self.assertAlmostEqual(result, 0.5381311, places=6)
 
         result = self.evaluate(loss(targets, logits, weights * 2.))
         self.assertAlmostEqual(result, 0.5381311 * 2, places=6)
@@ -86,7 +86,7 @@ class TestCrossEntropyLoss(test_combinations.TestCase):
         loss = CrossEntropyLoss(from_logits=True)
         result = self.evaluate(loss(targets, logits))
 
-        self.assertAlmostEqual(result, 5.349822)
+        self.assertAlmostEqual(result, 5.349822, places=6)
 
     def test_batch(self):
         probs = np.random.rand(2, 224, 224, 1).astype('float32')

segme/loss/tests/test_laplacian_pyramid.py
@@ -210,7 +210,7 @@ class TestLaplacianPyramidLoss(test_combinations.TestCase):
 
         loss = LaplacianPyramidLoss(levels=4, size=5, sigma=1.056, residual=True)
         result = self.evaluate(loss(targets, probs))
-        self.assertAlmostEqual(result, 7.6406145, places=6)  # with residual
+        self.assertAlmostEqual(result, 7.6406145, places=5)  # with residual
 
     def test_weight(self):
         logits = tf.constant([
@@ -232,13 +232,13 @@ class TestLaplacianPyramidLoss(test_combinations.TestCase):
         loss = LaplacianPyramidLoss(levels=2)
 
         result = self.evaluate(loss(targets[:, :, :32], logits[:, :, :32]))
-        self.assertAlmostEqual(result, 0.93986857)
+        self.assertAlmostEqual(result, 0.93986857, places=6)
 
         result = self.evaluate(loss(targets, logits, weights))
-        self.assertAlmostEqual(result, 0.5371178)
+        self.assertAlmostEqual(result, 0.5371178, places=6)
 
         result = self.evaluate(loss(targets, logits, weights * 2.))
-        self.assertAlmostEqual(result, 0.5371178 * 2.)
+        self.assertAlmostEqual(result, 0.5371178 * 2., places=6)
 
     def test_multi(self):
         logits = tf.constant([
@@ -260,7 +260,7 @@ class TestLaplacianPyramidLoss(test_combinations.TestCase):
         loss = LaplacianPyramidLoss(levels=1)
 
         result = self.evaluate(loss(targets, logits, weights))
-        self.assertAlmostEqual(result, 1.0270966)
+        self.assertAlmostEqual(result, 1.0270966, places=6)
 
     def test_batch(self):
         probs = np.random.rand(2, 128, 128, 3).astype('float32')

segme/loss/tests/test_mean_absolute.py
@@ -71,10 +71,10 @@ class TestMeanAbsoluteError(test_combinations.TestCase):
         loss = MeanAbsoluteError(from_logits=True)
 
         result = self.evaluate(loss(targets[:, :, :32], logits[:, :, :32]))
-        self.assertAlmostEqual(result, 0.39642578)
+        self.assertAlmostEqual(result, 0.39642578, places=6)
 
         result = self.evaluate(loss(targets, logits, weights))
-        self.assertAlmostEqual(result, 0.16849478)
+        self.assertAlmostEqual(result, 0.16849478, places=6)
 
         result = self.evaluate(loss(targets, logits, weights * 2.))
         self.assertAlmostEqual(result, 0.16849478 * 2, places=6)
@@ -86,7 +86,7 @@ class TestMeanAbsoluteError(test_combinations.TestCase):
         loss = MeanAbsoluteError(from_logits=True)
         result = self.evaluate(loss(targets, logits))
 
-        self.assertAlmostEqual(result, 0.51633525)
+        self.assertAlmostEqual(result, 0.51633525, places=6)
 
     def test_batch(self):
         probs = np.random.rand(2, 224, 224, 1).astype('float32')

segme/loss/tests/test_mean_squared.py
@@ -61,7 +61,7 @@ class TestMeanSquaredError(test_combinations.TestCase):
         loss = MeanSquaredError(from_logits=True)
         result = self.evaluate(loss(targets, logits))
 
-        self.assertAlmostEqual(result, 0.32667673)
+        self.assertAlmostEqual(result, 0.32667673, places=6)
 
     def test_weight(self):
         logits = tf.tile(BINARY_LOGITS, [1, 16, 16, 1])
@@ -71,10 +71,10 @@ class TestMeanSquaredError(test_combinations.TestCase):
         loss = MeanSquaredError(from_logits=True)
 
         result = self.evaluate(loss(targets[:, :, :32], logits[:, :, :32]))
-        self.assertAlmostEqual(result, 0.32667673)
+        self.assertAlmostEqual(result, 0.32667673, places=6)
 
         result = self.evaluate(loss(targets, logits, weights))
-        self.assertAlmostEqual(result, 0.12487063)
+        self.assertAlmostEqual(result, 0.12487063, places=6)
 
         result = self.evaluate(loss(targets, logits, weights * 2.))
         self.assertAlmostEqual(result, 0.12487063 * 2, places=6)
@@ -86,7 +86,7 @@ class TestMeanSquaredError(test_combinations.TestCase):
         loss = MeanSquaredError(from_logits=True)
         result = self.evaluate(loss(targets, logits))
 
-        self.assertAlmostEqual(result, 0.42551875)
+        self.assertAlmostEqual(result, 0.42551875, places=6)
 
     def test_batch(self):
         probs = np.random.rand(2, 224, 224, 1).astype('float32')

segme/loss/tests/test_structural_similarity.py
@@ -50,7 +50,7 @@ class TestSsimLevel(test_combinations.TestCase):
         result = tf.reduce_mean(result)
         result = self.evaluate(result)
 
-        self.assertAlmostEqual(0.5322474241256714, result, places=6)
+        self.assertAlmostEqual(0.5322474241256714, result, places=5)
 
 
 @test_combinations.run_all_keras_modes
@@ -231,7 +231,7 @@ class TestStructuralSimilarityLoss(test_combinations.TestCase):
         result = loss(targets, probs)
         result = self.evaluate(result)
 
-        self.assertAlmostEqual(result, 0.8302058)  # 0.8249481320381165 when compensation = 1
+        self.assertAlmostEqual(result, 0.8302058, places=6)  # 0.8249481320381165 when compensation = 1
 
     def test_weight(self):
         logits = tf.constant([
@@ -253,13 +253,13 @@ class TestStructuralSimilarityLoss(test_combinations.TestCase):
         loss = StructuralSimilarityLoss(factors=(0.5,), size=2)
 
         result = self.evaluate(loss(targets[:, :, :32], logits[:, :, :32]))
-        self.assertAlmostEqual(result, 0.46263173)
+        self.assertAlmostEqual(result, 0.46263173, places=6)
 
         result = self.evaluate(loss(targets, logits, weights))
-        self.assertAlmostEqual(result, 0.62305105)
+        self.assertAlmostEqual(result, 0.62305105, places=6)
 
         result = self.evaluate(loss(targets, logits, weights * 2.))
-        self.assertAlmostEqual(result, 0.4669136)
+        self.assertAlmostEqual(result, 0.4669136, places=6)
 
     def test_multi(self):
         logits = tf.constant([