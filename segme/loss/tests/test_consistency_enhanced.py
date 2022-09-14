Precision fix #2

segme/loss/tests/test_consistency_enhanced.py
@@ -76,7 +76,7 @@ class TestConsistencyEnhancedLoss(test_combinations.TestCase):
         loss = ConsistencyEnhancedLoss(from_logits=True)
         result = self.evaluate(loss(MULTI_TARGETS, MULTI_LOGITS))
 
-        self.assertAlmostEqual(result, 0.6837686)
+        self.assertAlmostEqual(result, 0.6837686, places=6)
 
     def test_batch(self):
         probs = np.random.rand(2, 224, 224, 1).astype('float32')

segme/loss/tests/test_laplace_edge.py
@@ -59,24 +59,24 @@ class TestLaplaceEdgeCrossEntropy(test_combinations.TestCase):
         result = self.evaluate(loss(BINARY_TARGETS, BINARY_LOGITS))
 
         # self.assertAlmostEqual(result, 3.879105925) # for zero padding and 1-channel BCE
-        self.assertAlmostEqual(result, 4.122532, places=5)
+        self.assertAlmostEqual(result, 4.122532, places=4)
 
     def test_weight(self):
         loss = LaplaceEdgeCrossEntropy(from_logits=True)
 
         result = self.evaluate(loss(BINARY_TARGETS[:, :, :2], BINARY_LOGITS[:, :, :2]))
-        self.assertAlmostEqual(result, 2.8783655, places=6)
+        self.assertAlmostEqual(result, 2.8783655, places=5)
 
         result = self.evaluate(loss(BINARY_TARGETS, BINARY_LOGITS, BINARY_WEIGHTS))
-        self.assertAlmostEqual(result, 1.5061817, places=6)
+        self.assertAlmostEqual(result, 1.5061817, places=5)
 
         result = self.evaluate(loss(BINARY_TARGETS, BINARY_LOGITS, BINARY_WEIGHTS * 2.))
-        self.assertAlmostEqual(result, 1.5061817 * 2, places=6)
+        self.assertAlmostEqual(result, 1.5061817 * 2, places=5)
 
     def test_multi(self):
         loss = LaplaceEdgeCrossEntropy(from_logits=True)
         result = self.evaluate(loss(MULTI_TARGETS, MULTI_LOGITS))
-        self.assertAlmostEqual(result, 3.2714694, places=6)
+        self.assertAlmostEqual(result, 3.2714694, places=5)
 
     def test_batch(self):
         probs = np.random.rand(2, 224, 224, 1).astype('float32')

segme/loss/tests/test_structural_similarity.py
@@ -231,7 +231,7 @@ class TestStructuralSimilarityLoss(test_combinations.TestCase):
         result = loss(targets, probs)
         result = self.evaluate(result)
 
-        self.assertAlmostEqual(result, 0.8302058, places=6)  # 0.8249481320381165 when compensation = 1
+        self.assertAlmostEqual(result, 0.8302058, places=5)  # 0.8249481320381165 when compensation = 1
 
     def test_weight(self):
         logits = tf.constant([