2795657b75945abd2ceb80146e6bdc49689ef131
Shkarupa Alex <shkarupa.alex@gmail.com>
2022-12-02 15:13:32 +0300

    Revert gamma estimation

--- segme/common/drop.py
+++ segme/common/drop.py
@@ -78,14 +78,9 @@ class DropBlock(layers.Layer):
         return outputs
 
     def drop(self, inputs):
-        shape = tf.shape(inputs)
-        fsize = tf.reduce_mean(shape[1:3])
-        fsize = tf.cast(fsize, self.compute_dtype)
-        bsize = float(self.size)
+        gamma = (self.rate / self.size ** 2)
 
-        gamma = (self.rate / bsize ** 2) * (fsize / (fsize - bsize + 1.)) ** 2
-
-        mask = tf.random.uniform(shape, dtype=self.compute_dtype)
+        mask = tf.random.uniform(tf.shape(inputs), dtype=self.compute_dtype)
         mask = tf.cast(mask < gamma, self.compute_dtype)
         mask = 1. - tf.nn.max_pool2d(mask, self.size, 1, 'SAME')
         mask = mask / tf.reduce_mean(mask, axis=[1, 2], keepdims=True)
