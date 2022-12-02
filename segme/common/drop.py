62a611a2c7cba82e0e23898e997ef820bc9e05fd
Shkarupa Alex <shkarupa.alex@gmail.com>
2022-12-02 12:11:49 +0300

    DropBlock

--- segme/common/drop.py
+++ segme/common/drop.py
@@ -48,3 +48,56 @@ class DropPath(layers.Layer):
         config.update({'rate': self.rate})
 
         return config
+
+
+@register_keras_serializable(package='SegMe>Common')
+class DropBlock(layers.Layer):
+    """ Proposed in: https://arxiv.org/pdf/1810.12890.pdf """
+
+    def __init__(self, rate, size, **kwargs):
+        super().__init__(**kwargs)
+        self.input_spec = layers.InputSpec(ndim=4)
+
+        if not 0. <= rate <= 1.:
+            raise ValueError(f'Invalid value {rate} received for `rate`. Expected a value between 0 and 1.')
+        if size < 1:
+            raise ValueError(f'Invalid value {size} received for `size`. Expected a value above 0.')
+
+        self.rate = rate
+        self.size = size
+
+    def call(self, inputs, training=None, **kwargs):
+        if 0. == self.rate:
+            return inputs
+
+        if training is None:
+            training = backend.learning_phase()
+
+        outputs = smart_cond(training, lambda: self.drop(inputs), lambda: tf.identity(inputs))
+
+        return outputs
+
+    def drop(self, inputs):
+        gamma = self.rate / (self.size ** 2)
+
+        mask = tf.random.uniform(tf.shape(inputs)[:3])[..., None]
+        mask = tf.cast(mask < gamma, self.compute_dtype)
+        mask = 1. - tf.nn.max_pool2d(mask, self.size, 1, 'SAME')
+        mask = mask / tf.reduce_mean(mask, axis=[1, 2], keepdims=True)
+
+        outputs = inputs * mask
+
+        return outputs
+
+    @shape_type_conversion
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            'rate': self.rate,
+            'size': self.size
+        })
+
+        return config
--- segme/common/tests/test_drop.py
+++ segme/common/tests/test_drop.py
@@ -1,7 +1,8 @@
+import numpy as np
 import tensorflow as tf
 from keras.mixed_precision import policy as mixed_precision
 from keras.testing_infra import test_combinations, test_utils
-from segme.common.drop import DropPath
+from segme.common.drop import DropPath, DropBlock
 
 
 @test_combinations.run_all_keras_modes
@@ -36,12 +37,61 @@ class TestDropPath(test_combinations.TestCase):
         mixed_precision.set_global_policy('mixed_float16')
         test_utils.layer_test(
             DropPath,
-            kwargs={'rate': 0.},
+            kwargs={'rate': 0.1},
             input_shape=[2, 16, 3],
             input_dtype='float16',
             expected_output_shape=[None, 16, 3],
             expected_output_dtype='float16'
         )
+
+
+@test_combinations.run_all_keras_modes
+class TestDropBlock(test_combinations.TestCase):
+    def setUp(self):
+        super(TestDropBlock, self).setUp()
+        self.default_policy = mixed_precision.global_policy()
+
+    def tearDown(self):
+        super(TestDropBlock, self).tearDown()
+        mixed_precision.set_global_policy(self.default_policy)
+
+    def test_layer(self):
+        test_utils.layer_test(
+            DropBlock,
+            kwargs={'rate': 0., 'size': 2},
+            input_shape=[2, 16, 3, 3],
+            input_dtype='float32',
+            expected_output_shape=[None, 16, 3, 3],
+            expected_output_dtype='float32'
+        )
+        test_utils.layer_test(
+            DropBlock,
+            kwargs={'rate': 0.2, 'size': 1},
+            input_shape=[2, 16, 16, 4],
+            input_dtype='float32',
+            expected_output_shape=[None, 16, 16, 4],
+            expected_output_dtype='float32'
+        )
+
+    def test_fp16(self):
+        mixed_precision.set_global_policy('mixed_float16')
+        test_utils.layer_test(
+            DropBlock,
+            kwargs={'rate': 0.1, 'size': 3},
+            input_shape=[2, 16, 3, 1],
+            input_dtype='float16',
+            expected_output_shape=[None, 16, 3, 1],
+            expected_output_dtype='float16'
+        )
+
+    def test_mean(self):
+        inputs = np.random.uniform(size=[4, 32, 32, 3]).astype('float32')
+
+        outputs = DropBlock(0.2, 3)(inputs, training=True)
+        outputs = self.evaluate(outputs)
+
+        self.assertNotAllClose(inputs, outputs)
+        self.assertAllClose(inputs.mean(axis=(1, 2)), outputs.mean(axis=(1, 2)), atol=5e-2)
 
 
 if __name__ == '__main__':
