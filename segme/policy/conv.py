a082454ccf0d30c43be0fac225692147a18887ba
Shkarupa Alex <shkarupa.alex@gmail.com>
2022-12-26 10:41:01 +0300

    DW convs

--- segme/policy/conv.py
+++ segme/policy/conv.py
@@ -17,8 +17,8 @@ class FixedConv(layers.Conv2D):
                  bias_constraint=None, **kwargs):
         kwargs.pop('groups', None)
         super().__init__(filters=filters, kernel_size=kernel_size, strides=strides, padding=padding,
-                         data_format=data_format, dilation_rate=dilation_rate, groups=1, activation=activation,
-                         use_bias=use_bias, kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
+                         data_format=data_format, dilation_rate=dilation_rate, activation=activation, use_bias=use_bias,
+                         kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
                          kernel_regularizer=kernel_regularizer, bias_regularizer=bias_regularizer,
                          activity_regularizer=activity_regularizer, kernel_constraint=kernel_constraint,
                          bias_constraint=bias_constraint, **kwargs)
@@ -54,7 +54,7 @@ class FixedConv(layers.Conv2D):
         return config
 
 
-@CONVOLUTIONS.register('dwconv')
+@CONVOLUTIONS.register('dw_conv')
 @register_keras_serializable(package='SegMe>Policy>Conv')
 class FixedDepthwiseConv(layers.DepthwiseConv2D):
     def __init__(self, kernel_size, strides=(1, 1), padding='valid', data_format=None, dilation_rate=(1, 1),
@@ -127,8 +127,8 @@ class StandardizedConv(FixedConv):
                  kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None,
                  bias_constraint=None, standardize_l1=1e-4, **kwargs):
         super().__init__(filters=filters, kernel_size=kernel_size, strides=strides, padding=padding,
-                         data_format=data_format, dilation_rate=dilation_rate, groups=1, activation=activation,
-                         use_bias=use_bias, kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
+                         data_format=data_format, dilation_rate=dilation_rate, activation=activation, use_bias=use_bias,
+                         kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
                          kernel_regularizer=self._standardized_regularizer, bias_regularizer=bias_regularizer,
                          activity_regularizer=activity_regularizer, kernel_constraint=kernel_constraint,
                          bias_constraint=bias_constraint, **kwargs)
@@ -171,6 +171,60 @@ class StandardizedConv(FixedConv):
         return config
 
 
+@CONVOLUTIONS.register('dw_stdconv')
+@register_keras_serializable(package='SegMe>Policy>Conv')
+class StandardizedDepthwiseConv(FixedDepthwiseConv):
+    """Implements https://arxiv.org/abs/1903.10520"""
+
+    def __init__(self, kernel_size, strides=(1, 1), padding='valid', data_format=None, dilation_rate=(1, 1),
+                 activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros',
+                 kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None,
+                 bias_constraint=None, standardize_l1=1e-4, **kwargs):
+        super().__init__(kernel_size=kernel_size, strides=strides, padding=padding, data_format=data_format,
+                         dilation_rate=dilation_rate, activation=activation, use_bias=use_bias,
+                         kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
+                         kernel_regularizer=self._standardized_regularizer, bias_regularizer=bias_regularizer,
+                         activity_regularizer=activity_regularizer, kernel_constraint=kernel_constraint,
+                         bias_constraint=bias_constraint, **kwargs)
+
+        self._kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.standardize_l1 = standardize_l1
+
+    def _standardize_kernel(self, kernel, dtype=None):
+        kernel = tf.cast(kernel, self.dtype)
+        mean, var = tf.nn.moments(kernel, axes=[0, 1], keepdims=True)
+        kernel = tf.nn.batch_normalization(kernel, mean, var, None, None, 1e-5)
+        kernel = tf.cast(kernel, dtype or self.compute_dtype)
+
+        return kernel
+
+    def _standardized_regularizer(self, kernel):
+        expected = self._standardize_kernel(kernel, kernel.dtype)
+        expected = tf.stop_gradient(expected)
+
+        loss = tf.reduce_mean(tf.abs(kernel - expected), axis=[0, 1])
+        loss = self.standardize_l1 * tf.reduce_sum(loss)
+
+        if self._kernel_regularizer is not None:
+            loss += self._kernel_regularizer(kernel)
+
+        return loss
+
+    def _conv_op(self, inputs, kernel):
+        kernel = self._standardize_kernel(kernel)
+
+        return super()._conv_op(inputs, kernel)
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            'kernel_regularizer': regularizers.serialize(self._kernel_regularizer),
+            'standardize_l1': self.standardize_l1
+        })
+
+        return config
+
+
 @CONVOLUTIONS.register('snconv')
 @register_keras_serializable(package='SegMe>Policy>Conv')
 class SpectralConv(FixedConv):
@@ -187,6 +241,8 @@ class SpectralConv(FixedConv):
                          activity_regularizer=activity_regularizer, kernel_constraint=kernel_constraint,
                          bias_constraint=bias_constraint, **kwargs)
 
+        if power_iterations < 1:
+            raise ValueError('Number of iterations must be greater then 0.')
         self.power_iterations = power_iterations
 
     @shape_type_conversion
@@ -217,6 +273,84 @@ class SpectralConv(FixedConv):
         u = tf.stop_gradient(u)
 
         kernel_update = self.kernel.assign(kernel, read_value=False)
+        u_update = self.u.assign(u, read_value=False)
+        with tf.control_dependencies([kernel_update, u_update]):
+            outputs = tf.identity(inputs)
+
+        return outputs
+
+    def call(self, inputs, training=None):
+        if training is None:
+            training = backend.learning_phase()
+
+        outputs = smart_cond(
+            training,
+            lambda: self.before_train(inputs),
+            lambda: tf.identity(inputs))
+
+        outputs = super().call(outputs)
+
+        return outputs
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({'power_iterations': self.power_iterations})
+
+        return config
+
+
+@CONVOLUTIONS.register('dw_snconv')
+@register_keras_serializable(package='SegMe>Policy>Conv')
+class SpectralDepthwiseConv(FixedDepthwiseConv):
+    """Implements https://arxiv.org/abs/1802.05957"""
+
+    def __init__(self, kernel_size, strides=(1, 1), padding='valid', data_format=None, dilation_rate=(1, 1),
+                 activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros',
+                 kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None,
+                 bias_constraint=None, power_iterations=1, **kwargs):
+        super().__init__(kernel_size=kernel_size, strides=strides, padding=padding, data_format=data_format,
+                         dilation_rate=dilation_rate, activation=activation, use_bias=use_bias,
+                         kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
+                         kernel_regularizer=kernel_regularizer, bias_regularizer=bias_regularizer,
+                         activity_regularizer=activity_regularizer, kernel_constraint=kernel_constraint,
+                         bias_constraint=bias_constraint, **kwargs)
+
+        if power_iterations < 1:
+            raise ValueError('Number of iterations must be greater then 0.')
+        self.power_iterations = power_iterations
+
+    @shape_type_conversion
+    def build(self, input_shape):
+        self.channels = input_shape[-1]
+        if self.channels is None:
+            raise ValueError('Channel dimension of the inputs should be defined. Found `None`.')
+
+        self.u = self.add_weight(
+            shape=(1, self.channels),
+            initializer=initializers.TruncatedNormal(stddev=0.02),
+            trainable=False,
+            name='sn_u',
+            dtype=self.dtype,
+        )
+
+        super().build(input_shape)
+
+    def before_train(self, inputs):
+        kernel = tf.cast(self.depthwise_kernel, self.dtype)
+        u = tf.cast(self.u, self.dtype)
+
+        w = tf.reshape(kernel, [-1, self.channels])
+
+        for _ in range(self.power_iterations):
+            v = tf.math.l2_normalize(tf.matmul(u, w, transpose_b=True))
+            u = tf.math.l2_normalize(tf.matmul(v, w))
+
+        sigma = tf.matmul(tf.matmul(v, w), u, transpose_b=True)
+        kernel = tf.reshape(kernel / sigma, self.depthwise_kernel.shape)
+        kernel = tf.stop_gradient(kernel)
+        u = tf.stop_gradient(u)
+
+        kernel_update = self.depthwise_kernel.assign(kernel, read_value=False)
         u_update = self.u.assign(u, read_value=False)
         with tf.control_dependencies([kernel_update, u_update]):
             outputs = tf.identity(inputs)
--- segme/policy/tests/test_conv.py
+++ segme/policy/tests/test_conv.py
@@ -5,7 +5,8 @@ import unittest
 from keras import layers
 from keras.testing_infra import test_combinations, test_utils
 from keras.mixed_precision import policy as mixed_precision
-from segme.policy.conv import CONVOLUTIONS, FixedConv, FixedDepthwiseConv, StandardizedConv, SpectralConv
+from segme.policy.conv import CONVOLUTIONS, FixedConv, FixedDepthwiseConv, StandardizedConv, \
+    StandardizedDepthwiseConv, SpectralConv, SpectralDepthwiseConv
 
 
 class TestConvsRegistry(unittest.TestCase):
@@ -277,6 +278,83 @@ class TestStandardizedConv(test_combinations.TestCase):
 
 
 @test_combinations.run_all_keras_modes
+class TestStandardizedDepthwiseConv(test_combinations.TestCase):
+    def setUp(self):
+        super(TestStandardizedDepthwiseConv, self).setUp()
+        self.default_policy = mixed_precision.global_policy()
+
+    def tearDown(self):
+        super(TestStandardizedDepthwiseConv, self).tearDown()
+        mixed_precision.set_global_policy(self.default_policy)
+
+    def test_layer(self):
+        test_utils.layer_test(
+            StandardizedDepthwiseConv,
+            kwargs={'kernel_size': 1, 'strides': 1, 'padding': 'valid'},
+            input_shape=[2, 16, 16, 8],
+            input_dtype='float32',
+            expected_output_shape=[None, 16, 16, 8],
+            expected_output_dtype='float32'
+        )
+
+    def test_fp16(self):
+        mixed_precision.set_global_policy('mixed_float16')
+        result = test_utils.layer_test(
+            StandardizedDepthwiseConv,
+            kwargs={'kernel_size': 3, 'strides': 2, 'padding': 'same'},
+            input_shape=[2, 16, 16, 8],
+            input_dtype='float16',
+            expected_output_shape=[None, 8, 8, 8],
+            expected_output_dtype='float16'
+        )
+        self.assertTrue(np.all(np.isfinite(result)))
+
+    def test_value(self):
+        inputs = np.array([
+            0.298, -0.336, 4.725, -0.393, -1.951, -3.269, -2.453, -0.043, 0.25, -4.571, -2.143, 2.744, 1.483, 3.229,
+            1.472, -1.802, 3.146, -0.048, 1.407, 1.315, -0.823, 0.763, -0.103, 0.295, 1.507, -3.52, -1.55, -2.573,
+            0.929, 1.649, 1.545, -0.365, 1.845, 1.208, -0.829, -3.652], 'float32').reshape((1, 3, 4, 3))
+        kernel = np.array([
+            0.307, -0.094, 0.031, -0.301, -0.164, -0.073, 0.07, -0.167, 0.267, -0.128, -0.226, -0.181, -0.248, -0.05,
+            0.056, -0.535, 0.221, -0.04, 0.521, -0.285, -0.323, 0.094, 0.362, -0.022, -0.097, -0.054, -0.084],
+            'float32').reshape((3, 3, 3, 1))
+        bias = np.zeros((3,), 'float32')
+        expected = np.array(
+            [-0.32465044, 6.2342463, 4.557652, -5.863132, -3.0357158, 1.6686258], 'float32').reshape((1, 1, 2, 3))
+
+        layer = StandardizedDepthwiseConv(3)
+        layer.build(inputs.shape)
+        layer.set_weights([kernel, bias])
+
+        result = layer(inputs, training=True)
+        result = self.evaluate(result)
+        self.assertAllClose(expected, result)
+
+        result = layer(inputs)
+        result = self.evaluate(result)
+        self.assertAllClose(expected, result)
+
+    def test_batch(self):
+        inputs = np.random.normal(size=(32, 16, 16, 64)) * 10.
+        layer = StandardizedDepthwiseConv(3)
+
+        expected = layer(inputs, training=True)
+        expected = self.evaluate(expected)
+
+        result = layer(inputs, training=False)
+        result = self.evaluate(result)
+        self.assertAllClose(expected, result)
+
+        result = []
+        for i in range(inputs.shape[0]):
+            result1 = layer(inputs[i:i + 1], training=False)
+            result1 = self.evaluate(result1)
+            result.append(result1)
+        result = np.concatenate(result, axis=0)
+        self.assertAllClose(expected, result, atol=1e-4)
+
+
+@test_combinations.run_all_keras_modes
 class TestSpectralConv(test_combinations.TestCase):
     def setUp(self):
         super(TestSpectralConv, self).setUp()
@@ -336,6 +414,84 @@ class TestSpectralConv(test_combinations.TestCase):
     def test_batch(self):
         inputs = np.random.normal(size=(32, 16, 16, 64)) * 10.
         layer = SpectralConv(64, 3)
+
+        expected = layer(inputs, training=True)
+        expected = self.evaluate(expected)
+
+        result = layer(inputs, training=False)
+        result = self.evaluate(result)
+        self.assertAllClose(expected, result)
+
+        result = []
+        for i in range(inputs.shape[0]):
+            result1 = layer(inputs[i:i + 1], training=False)
+            result1 = self.evaluate(result1)
+            result.append(result1)
+        result = np.concatenate(result, axis=0)
+        self.assertAllClose(expected, result, atol=6e-5)
+
+
+@test_combinations.run_all_keras_modes
+class TestSpectralDepthwiseConv(test_combinations.TestCase):
+    def setUp(self):
+        super(TestSpectralDepthwiseConv, self).setUp()
+        self.default_policy = mixed_precision.global_policy()
+
+    def tearDown(self):
+        super(TestSpectralDepthwiseConv, self).tearDown()
+        mixed_precision.set_global_policy(self.default_policy)
+
+    def test_layer(self):
+        test_utils.layer_test(
+            SpectralDepthwiseConv,
+            kwargs={'kernel_size': 1, 'strides': 1, 'padding': 'valid', 'power_iterations': 2},
+            input_shape=[2, 16, 16, 8],
+            input_dtype='float32',
+            expected_output_shape=[None, 16, 16, 8],
+            expected_output_dtype='float32'
+        )
+
+    def test_fp16(self):
+        mixed_precision.set_global_policy('mixed_float16')
+        result = test_utils.layer_test(
+            SpectralDepthwiseConv,
+            kwargs={'kernel_size': 3, 'strides': 2, 'padding': 'same', 'power_iterations': 1},
+            input_shape=[2, 16, 16, 8],
+            input_dtype='float16',
+            expected_output_shape=[None, 8, 8, 8],
+            expected_output_dtype='float16'
+        )
+        self.assertTrue(np.all(np.isfinite(result)))
+
+    def test_value(self):
+        inputs = np.array([
+            0.298, -0.336, 4.725, -0.393, -1.951, -3.269, -2.453, -0.043, 0.25, -4.571, -2.143, 2.744, 1.483, 3.229,
+            1.472, -1.802, 3.146, -0.048, 1.407, 1.315, -0.823, 0.763, -0.103, 0.295, 1.507, -3.52, -1.55, -2.573,
+            0.929, 1.649, 1.545, -0.365, 1.845, 1.208, -0.829, -3.652], 'float32').reshape((1, 3, 4, 3))
+        kernel = np.array([
+            0.307, -0.094, 0.031, -0.301, -0.164, -0.073, 0.07, -0.167, 0.267, -0.128, -0.226, -0.181, -0.248, -0.05,
+            0.056, -0.535, 0.221, -0.04, 0.521, -0.285, -0.323, 0.094, 0.362, -0.022, -0.097, -0.054, -0.084],
+            'float32').reshape((3, 3, 3, 1))
+        bias = np.zeros((3,), 'float32')
+        u = np.array([[-0.008, 0.009, -0.002]], 'float32')
+        expected = np.array([
+            -0.06966147, 1.2172067, 0.5698621, -1.6727608, -0.65479755, 0.32279092], 'float32').reshape((1, 1, 2, 3))
+
+        layer = SpectralDepthwiseConv(3)
+        layer.build(inputs.shape)
+        layer.set_weights([kernel, bias, u])
+
+        result = layer(inputs, training=True)
+        result = self.evaluate(result)
+        self.assertAllClose(expected, result)
+
+        result = layer(inputs)
+        result = self.evaluate(result)
+        self.assertAllClose(expected, result)
+
+    def test_batch(self):
+        inputs = np.random.normal(size=(32, 16, 16, 64)) * 10.
+        layer = SpectralDepthwiseConv(3)
 
         expected = layer(inputs, training=True)
         expected = self.evaluate(expected)
