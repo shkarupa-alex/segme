01112d302396442987ceeadde413d8b748c3e02e
Shkarupa Alex <shkarupa.alex@gmail.com>
2023-07-10 22:52:34 +0300

    Drop adaptive max pool, add static branch to adaptive avg pool

--- segme/common/adppool.py
+++ segme/common/adppool.py
@@ -1,19 +1,4 @@
-# Taken from https://github.com/tensorflow/addons/blob/master/tensorflow_addons/layers/adaptive_pooling.py
-#
-# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
+import numpy as np
 import tensorflow as tf
 from keras import layers
 from keras.saving import register_keras_serializable
@@ -23,89 +8,146 @@ from keras.src.utils.conv_utils import normalize_tuple
 
 
 @register_keras_serializable(package='SegMe>Common')
-class AdaptivePooling(layers.Layer):
-    # With workaround for https://github.com/tensorflow/addons/pull/2322
-
-    def __init__(self, reduce_function, output_size, **kwargs):
+class AdaptiveAveragePooling(layers.Layer):
+    def __init__(self, output_size, **kwargs):
         super().__init__(**kwargs)
         self.input_spec = layers.InputSpec(ndim=4)
-        self.reduce_function = reduce_function
         self.output_size = normalize_tuple(output_size, 2, 'output_size')
-
-    def case_global(self, inputs):
-        return self.reduce_function(inputs, axis=[1, 2], keepdims=True)
-
-    def case_divisible(self, inputs):
-        split_x = tf.split(inputs, self.output_size[0], axis=1)
-        split_x = tf.stack(split_x, axis=1)
-        split_y = tf.split(split_x, self.output_size[1], axis=3)
-        split_y = tf.stack(split_y, axis=3)
-
-        outputs = self.reduce_function(split_y, axis=[2, 4])
-
-        return outputs
-
-    def case_nondivisible(self, inputs):
-        inputs_shape = tf.shape(inputs)
-        inputs_shape1, inputs_shape2 = inputs_shape[1], inputs_shape[2]
-        start_points_x = tf.cast(
-            (tf.range(self.output_size[0], dtype=tf.float32)
-             * tf.cast(inputs_shape1 / self.output_size[0], tf.float32)),
-            tf.int32)
-        end_points_x = tf.cast(
-            tf.math.ceil(
-                (tf.range(self.output_size[0], dtype=tf.float32) + 1)
-                * tf.cast(inputs_shape1 / self.output_size[0], tf.float32)),
-            tf.int32)
-        start_points_y = tf.cast(
-            (tf.range(self.output_size[1], dtype=tf.float32)
-             * tf.cast(inputs_shape2 / self.output_size[1], tf.float32)),
-            tf.int32)
-        end_points_y = tf.cast(
-            tf.math.ceil((tf.range(self.output_size[1], dtype=tf.float32) + 1)
-                         * tf.cast(inputs_shape2 / self.output_size[1], tf.float32)),
-            tf.int32)
-
-        pooled = []
-        for idx in range(self.output_size[0]):
-            pooled.append(self.reduce_function(
-                inputs[:, start_points_x[idx]: end_points_x[idx], :, :],
-                axis=1,
-                keepdims=True,
-            ))
-        x_pooled = tf.concat(pooled, axis=1)
-
-        pooled = []
-        for idx in range(self.output_size[1]):
-            pooled.append(self.reduce_function(
-                x_pooled[:, :, start_points_y[idx]: end_points_y[idx], :],
-                axis=2,
-                keepdims=True,
-            ))
-        y_pooled = tf.concat(pooled, axis=2)
-
-        return y_pooled
 
     def call(self, inputs, *args, **kwargs):
         if (1, 1) == self.output_size:
             return self.case_global(inputs)
 
-        height, width = inputs.shape[1:3]
-        if None in (height, width):
-            height, width = tf.unstack(tf.shape(inputs)[1:3])
+        if None in inputs.shape[1:3]:
+            return self.case_dynamic(inputs)
+
+        return self.case_static(inputs)
+
+    def case_global(self, inputs):
+        return tf.reduce_mean(inputs, axis=[1, 2], keepdims=True)
+
+    def case_static(self, inputs):
+        input_size = inputs.shape[1:3]
+
+        start_h = np.arange(self.output_size[0], dtype='float32') * input_size[0] / self.output_size[0]
+        start_h = start_h.astype('int32')
+        stop_h = (np.arange(self.output_size[0], dtype='float32') + 1) * input_size[0] / self.output_size[0]
+        stop_h = np.ceil(stop_h).astype('int32')
+        size_h = stop_h - start_h
+        over_h = stop_h[:-1] - start_h[1:]
+
+        start_w = np.arange(self.output_size[1], dtype='float32') * input_size[1] / self.output_size[1]
+        start_w = start_w.astype('int32')
+        stop_w = (np.arange(self.output_size[1], dtype=np.float32) + 1) * input_size[1] / self.output_size[1]
+        stop_w = np.ceil(stop_w).astype('int32')
+        size_w = stop_w - start_w
+        over_w = stop_w[:-1] - start_w[1:]
+
+        kernels = np.array([size_h.max(), size_w.max()])
+        if (kernels < 1).any():
+            return self.case_static_nondivisible(inputs, input_size)
+        if np.unique(size_h[1:-1]).size > 1 or np.unique(size_w[1:-1]).size > 1:
+            return self.case_static_nondivisible(inputs, input_size)
+
+        if np.unique(over_h).size > 1 or np.unique(over_w).size > 1:
+            return self.case_static_nondivisible(inputs, input_size)
+        strides = kernels - np.array([over_h.max(), over_w.max()])
+
+        paddings = kernels - np.array([size_h.min(), size_w.min()])
+        paddings_ = [[0, 0], [paddings[0], paddings[0]], [paddings[1], paddings[1]], [0, 0]]
+
+        outputs = tf.pad(inputs, paddings_)
+        outputs = tf.nn.avg_pool(outputs, kernels, strides, 'VALID')
+
+        weights = tf.ones([1, input_size[0], input_size[1], 1], dtype=inputs.dtype)
+        weights = tf.pad(weights, paddings_)
+        weights = tf.nn.avg_pool(weights, kernels, strides, 'VALID')
+        outputs /= weights
+
+        return outputs
+
+    def case_static_nondivisible(self, inputs, input_size):
+        start_h = np.arange(self.output_size[0], dtype='float32') * input_size[0] / self.output_size[0]
+        start_h = start_h.astype('int32')
+        stop_h = (np.arange(self.output_size[0], dtype='float32') + 1) * input_size[0] / self.output_size[0]
+        stop_h = np.ceil(stop_h).astype('int32')
+
+        pooled_h = []
+        for idx in range(self.output_size[0]):
+            pooled_h.append(tf.reduce_mean(inputs[:, start_h[idx]: stop_h[idx]], axis=1, keepdims=True))
+        pooled_h = tf.concat(pooled_h, axis=1)
+
+        start_w = np.arange(self.output_size[1], dtype='float32') * input_size[1] / self.output_size[1]
+        start_w = start_w.astype('int32')
+        stop_w = (np.arange(self.output_size[1], dtype=np.float32) + 1) * input_size[1] / self.output_size[1]
+        stop_w = np.ceil(stop_w).astype('int32')
+
+        pooled_w = []
+        for idx in range(self.output_size[1]):
+            pooled_w.append(tf.reduce_mean(pooled_h[:, :, start_w[idx]: stop_w[idx]], axis=2, keepdims=True))
+        pooled_w = tf.concat(pooled_w, axis=2)
+
+        return pooled_w
+
+    def case_dynamic(self, inputs):
+        height, width = tf.unstack(tf.shape(inputs)[1:3])
         pad_h = (self.output_size[0] - height % self.output_size[0]) % self.output_size[0]
         pad_w = (self.output_size[1] - width % self.output_size[1]) % self.output_size[1]
 
-        # Hack to allow build non-divisible branch
+        # Hack to allow build divisible branch
         inputs_ = tf.pad(inputs, [[0, 0], [0, pad_h], [0, pad_w], [0, 0]])
 
         outputs = smart_cond(
-            (pad_h == 0) & (pad_w == 0),
-            lambda: self.case_divisible(inputs_),
-            lambda: self.case_nondivisible(inputs))
-        outputs.set_shape(self.compute_output_shape(inputs.shape))
+            pad_h + pad_w > 0,
+            lambda: self.case_dynamic_nondivisible(inputs, [height, width]),
+            lambda: self.case_dynamic_divisible(inputs_))
 
         return outputs
+
+    def case_dynamic_divisible(self, inputs):
+        split_x = tf.split(inputs, self.output_size[0], axis=1)
+        split_x = tf.stack(split_x, axis=1)
+        split_y = tf.split(split_x, self.output_size[1], axis=3)
+        split_y = tf.stack(split_y, axis=3)
+
+        outputs = tf.reduce_mean(split_y, axis=[2, 4])
+
+        return outputs
+
+    def case_dynamic_nondivisible(self, inputs, input_size):
+        start_h = tf.range(self.output_size[0], dtype='float32') * \
+                  tf.cast(input_size[0], 'float32') / self.output_size[0]
+        start_h = tf.cast(start_h, 'int32')
+        stop_h = (tf.range(self.output_size[0], dtype='float32') + 1) * \
+                 tf.cast(input_size[0], 'float32') / self.output_size[0]
+        stop_h = tf.cast(tf.math.ceil(stop_h), 'int32')
+
+        pooled_h = []
+        for idx in range(self.output_size[0]):
+            pooled_h.append(tf.reduce_mean(
+                inputs[:, start_h[idx]: stop_h[idx]],
+                axis=1,
+                keepdims=True,
+            ))
+        pooled_h = tf.concat(pooled_h, axis=1)
+
+        start_w = tf.range(self.output_size[1], dtype='float32') * \
+                  tf.cast(input_size[1], 'float32') / self.output_size[1]
+        start_w = tf.cast(start_w, 'int32')
+        stop_w = (tf.range(self.output_size[1], dtype='float32') + 1) * \
+                 tf.cast(input_size[1], 'float32') / self.output_size[1]
+        stop_w = tf.cast(tf.math.ceil(stop_w), 'int32')
+
+        pooled_w = []
+        for idx in range(self.output_size[1]):
+            pooled_w.append(tf.reduce_mean(
+                pooled_h[:, :, start_w[idx]: stop_w[idx]],
+                axis=2,
+                keepdims=True,
+            ))
+        pooled_w = tf.concat(pooled_w, axis=2)
+
+        return pooled_w
 
     @shape_type_conversion
     def compute_output_shape(self, input_shape):
@@ -116,15 +158,3 @@ class AdaptivePooling(layers.Layer):
         config.update({'output_size': self.output_size})
 
         return config
-
-
-@register_keras_serializable(package='SegMe>Common')
-class AdaptiveAveragePooling(AdaptivePooling):
-    def __init__(self, output_size, **kwargs):
-        super().__init__(reduce_function=tf.reduce_mean, output_size=output_size, **kwargs)
-
-
-@register_keras_serializable(package='SegMe>Common')
-class AdaptiveMaxPooling(AdaptivePooling):
-    def __init__(self, output_size, **kwargs):
-        super().__init__(reduce_function=tf.reduce_max, output_size=output_size, **kwargs)
--- segme/common/tests/test_adppool.py
+++ segme/common/tests/test_adppool.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 from keras import layers, mixed_precision, models
 from keras.src.testing_infra import test_combinations, test_utils
-from segme.common.adppool import AdaptiveAveragePooling, AdaptiveMaxPooling
+from segme.common.adppool import AdaptiveAveragePooling
 
 
 @test_combinations.run_all_keras_modes
@@ -99,54 +99,6 @@ class TestAdaptiveAveragePooling(test_combinations.TestCase):
         model = models.Model(inputs=inputs, outputs=outputs)
         model.compile('adam', 'mse', run_eagerly=test_utils.should_run_eagerly())
         model.fit(dataset)
-
-
-@test_combinations.run_all_keras_modes
-class TestAdaptiveMaxPooling(test_combinations.TestCase):
-    def setUp(self):
-        super(TestAdaptiveMaxPooling, self).setUp()
-        self.default_policy = mixed_precision.global_policy()
-
-    def tearDown(self):
-        super(TestAdaptiveMaxPooling, self).tearDown()
-        mixed_precision.set_global_policy(self.default_policy)
-
-    def test_layer(self):
-        test_utils.layer_test(
-            AdaptiveMaxPooling,
-            kwargs={'output_size': 2},
-            input_shape=[2, 16, 16, 3],
-            input_dtype='float32',
-            expected_output_shape=[None, 2, 2, 3],
-            expected_output_dtype='float32'
-        )
-        test_utils.layer_test(
-            AdaptiveMaxPooling,
-            kwargs={'output_size': (4, 3)},
-            input_shape=[2, 15, 16, 3],
-            input_dtype='float32',
-            expected_output_shape=[None, 4, 3, 3],
-            expected_output_dtype='float32'
-        )
-
-    def test_fp16(self):
-        mixed_precision.set_global_policy('mixed_float16')
-        test_utils.layer_test(
-            AdaptiveMaxPooling,
-            kwargs={'output_size': 2},
-            input_shape=[2, 16, 16, 3],
-            input_dtype='float16',
-            expected_output_shape=[None, 2, 2, 3],
-            expected_output_dtype='float16'
-        )
-        test_utils.layer_test(
-            AdaptiveMaxPooling,
-            kwargs={'output_size': (4, 3)},
-            input_shape=[2, 15, 16, 3],
-            input_dtype='float16',
-            expected_output_shape=[None, 4, 3, 3],
-            expected_output_dtype='float16'
-        )
 
 
 if __name__ == '__main__':
