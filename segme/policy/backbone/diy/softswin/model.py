cc419221b79daa44a09545b53d3a5c95f96d58b0
Shkarupa Alex <shkarupa.alex@gmail.com>
2023-08-28 11:24:39 +0300

    SoftSwin refactoring

--- segme/policy/backbone/diy/softswin/model.py
+++ segme/policy/backbone/diy/softswin/model.py
@@ -197,100 +197,102 @@ def SoftSwin(
     path_drops = np.linspace(0., path_drop, stem_depth + sum(stage_depths)).tolist()
     stem_drops, path_drops = path_drops[:stem_depth], path_drops[stem_depth:]
 
-    with cnapol.policy_scope('conv-gn-gelu'):
-        stem_filters = np.ceil(embed_dim / 4 / 8).astype('int32').item() * 8
-        x = Stem(stem_filters, stem_depth, path_gamma=stem_gammas, path_drop=stem_drops, name='stem')(x)
-        x = layers.Activation('linear', name='stem_out')(x)
+    stem_filters = np.ceil(embed_dim / 4 / 8).astype('int32').item() * 8
+    x = Stem(stem_filters, stem_depth, path_gamma=stem_gammas, path_drop=stem_drops, name='stem')(x)
+    x = layers.Activation('linear', name='stem_out')(x)
 
-    with cnapol.policy_scope('conv-ln-gelu'):
-        shift_counter = -1
-        for i, stage_depth in enumerate(stage_depths):
-            stage_window = min(current_window, current_size // 2 ** (i + 2))
-            num_heads = embed_dim // 2 ** (5 - i)
+    shift_counter = -1
+    for i, stage_depth in enumerate(stage_depths):
+        stage_window = min(current_window, current_size // 2 ** (i + 2))
+        num_heads = embed_dim // 2 ** (5 - i)
 
-            stage_gammas, path_gammas = path_gammas[:stage_depth], path_gammas[stage_depth:]
-            stage_drops, path_drops = path_drops[:stage_depth], path_drops[stage_depth:]
+        stage_gammas, path_gammas = path_gammas[:stage_depth], path_gammas[stage_depth:]
+        stage_drops, path_drops = path_drops[:stage_depth], path_drops[stage_depth:]
 
-            reduce_fused = 0 == i  # From EfficientNet2
-            reduce_ratio = embed_dim / stem_filters if 0 == i else 2
-            x = Reduce(
-                reduce_fused, expand_ratio=reduce_ratio, project_ratio=reduce_ratio, name=f'stage_{i}_reduce')(x)
+        reduce_fused = 0 == i  # From EfficientNet2
+        reduce_ratio = embed_dim / stem_filters if 0 == i else 2
+        x = Reduce(
+            reduce_fused, expand_ratio=reduce_ratio, project_ratio=reduce_ratio, name=f'stage_{i}_reduce')(x)
 
-            for j in range(stage_depth):
-                shift_counter += j % 2
-                shift_mode = shift_counter % 4 + 1 if j % 2 else 0
-                x = AttnBlock(
-                    stage_window, pretrain_window, num_heads, shift_mode, expand_ratio=expand_ratio,
-                    path_gamma=stage_gammas[j], path_drop=stage_drops[j], name=f'stage_{i}_attn_{j}')(x)
+        for j in range(stage_depth):
+            shift_counter += j % 2
+            shift_mode = shift_counter % 4 + 1 if j % 2 else 0
+            x = AttnBlock(
+                stage_window, pretrain_window, num_heads, shift_mode, expand_ratio=expand_ratio,
+                path_gamma=stage_gammas[j], path_drop=stage_drops[j], name=f'stage_{i}_attn_{j}')(x)
 
-            x = layers.Activation('linear', name=f'stage_{i}_out')(x)
+        x = layers.Activation('linear', name=f'stage_{i}_out')(x)
 
-        x = Norm(name='norm')(x)
+    x = Norm(name='norm')(x)
 
-        if include_top or pooling in {None, 'avg'}:
-            x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
-        elif pooling == 'max':
-            x = layers.GlobalMaxPooling2D(name='max_pool')(x)
-        else:
-            raise ValueError(f'Expecting pooling to be one of None/avg/max. Found: {pooling}')
+    if include_top or pooling in {None, 'avg'}:
+        x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+    elif pooling == 'max':
+        x = layers.GlobalMaxPooling2D(name='max_pool')(x)
+    else:
+        raise ValueError(f'Expecting pooling to be one of None/avg/max. Found: {pooling}')
 
-        imagenet_utils.validate_activation(classifier_activation, weights)
-        x = layers.Dense(classes, name='head')(x)
-        x = layers.Activation(classifier_activation, dtype='float32', name='pred')(x)
+    imagenet_utils.validate_activation(classifier_activation, weights)
+    x = layers.Dense(classes, name='head')(x)
+    x = layers.Activation(classifier_activation, dtype='float32', name='pred')(x)
 
-        if input_tensor is not None:
-            inputs = layer_utils.get_source_inputs(input_tensor)
-        else:
-            inputs = image
+    if input_tensor is not None:
+        inputs = layer_utils.get_source_inputs(input_tensor)
+    else:
+        inputs = image
 
-        model = models.Model(inputs, x, name=model_name)
+    model = models.Model(inputs, x, name=model_name)
 
-        weights_key = f'{model_name}_{cnapol.global_policy()}'
-        if 'imagenet' == weights and weights_key in WEIGHT_URLS:
-            weights_url = WEIGHT_URLS[weights_key]
-            weights_hash = WEIGHT_HASHES[weights_key]
-            weights_path = data_utils.get_file(origin=weights_url, file_hash=weights_hash, cache_subdir='soft_swin')
-            model.load_weights(weights_path)
-        elif weights is not None:
-            model.load_weights(weights)
+    weights_key = f'{model_name}_{cnapol.global_policy()}'
+    if 'imagenet' == weights and weights_key in WEIGHT_URLS:
+        weights_url = WEIGHT_URLS[weights_key]
+        weights_hash = WEIGHT_HASHES[weights_key]
+        weights_path = data_utils.get_file(origin=weights_url, file_hash=weights_hash, cache_subdir='soft_swin')
+        model.load_weights(weights_path)
+    elif weights is not None:
+        model.load_weights(weights)
 
-        if include_top:
-            return model
-
-        outputs = model.get_layer(name='norm').output
-        model = models.Model(inputs=inputs, outputs=outputs, name=model_name)
-
+    if include_top:
         return model
+
+    outputs = model.get_layer(name='norm').output
+    model = models.Model(inputs=inputs, outputs=outputs, name=model_name)
+
+    return model
 
 
 def SoftSwinTiny(
         embed_dim=96, stem_depth=2, stage_depths=(2, 2, 6, 2), pretrain_window=16, pretrain_size=256, **kwargs):
     # 28.0 14.3
-    return SoftSwin(
-        embed_dim=embed_dim, stem_depth=stem_depth, stage_depths=stage_depths, pretrain_window=pretrain_window,
-        pretrain_size=pretrain_size, model_name='soft_swin_tiny', **kwargs)
+    with cnapol.policy_scope('conv-ln-gelu'):
+        return SoftSwin(
+            embed_dim=embed_dim, stem_depth=stem_depth, stage_depths=stage_depths, pretrain_window=pretrain_window,
+            pretrain_size=pretrain_size, model_name='soft_swin_tiny', **kwargs)
 
 
 def SoftSwinSmall(
         embed_dim=96, stem_depth=2, stage_depths=(2, 2, 18, 2), pretrain_window=16, path_drop=0.3, pretrain_size=256,
         **kwargs):
     # 49.4 26.7
-    return SoftSwin(
-        embed_dim=embed_dim, stem_depth=stem_depth, stage_depths=stage_depths, pretrain_window=pretrain_window,
-        path_drop=path_drop, pretrain_size=pretrain_size, model_name='soft_swin_small', **kwargs)
+    with cnapol.policy_scope('conv-ln-gelu'):
+        return SoftSwin(
+            embed_dim=embed_dim, stem_depth=stem_depth, stage_depths=stage_depths, pretrain_window=pretrain_window,
+            path_drop=path_drop, pretrain_size=pretrain_size, model_name='soft_swin_small', **kwargs)
 
 
 def SoftSwinBase(
         embed_dim=128, stem_depth=3, stage_depths=(2, 2, 18, 2), current_window=24, pretrain_window=12, **kwargs):
     # 87.3 50.6 @ 256
-    return SoftSwin(
-        embed_dim=embed_dim, stem_depth=stem_depth, stage_depths=stage_depths, current_window=current_window,
-        pretrain_window=pretrain_window, model_name='soft_swin_base', **kwargs)
+    with cnapol.policy_scope('conv-ln-gelu'):
+        return SoftSwin(
+            embed_dim=embed_dim, stem_depth=stem_depth, stage_depths=stage_depths, current_window=current_window,
+            pretrain_window=pretrain_window, model_name='soft_swin_base', **kwargs)
 
 
 def SoftSwinLarge(
         embed_dim=192, stem_depth=4, stage_depths=(2, 2, 18, 2), current_window=24, pretrain_window=12, **kwargs):
     # 195.4 107.4 @ 256
-    return SoftSwin(
-        embed_dim=embed_dim, stem_depth=stem_depth, stage_depths=stage_depths, current_window=current_window,
-        pretrain_window=pretrain_window, model_name='soft_swin_large', **kwargs)
+    with cnapol.policy_scope('conv-ln-gelu'):
+        return SoftSwin(
+            embed_dim=embed_dim, stem_depth=stem_depth, stage_depths=stage_depths, current_window=current_window,
+            pretrain_window=pretrain_window, model_name='soft_swin_large', **kwargs)
