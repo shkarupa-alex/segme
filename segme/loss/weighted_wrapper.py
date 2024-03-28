6705580b4a46c8e23bf547fb6bb457ee8a801354
Shkarupa Alex <shkarupa.alex@gmail.com>
2024-03-28 21:16:23 +0300

    Loss wrapper imports refactoring

--- segme/loss/weighted_wrapper.py
+++ segme/loss/weighted_wrapper.py
@@ -1,7 +1,4 @@
-from tensorflow.python.autograph.core import ag_ctx
-from tensorflow.python.autograph.impl import api as autograph
-from tensorflow.python.eager import context
-from tensorflow.python.framework import tensor_util
+import tensorflow as tf
 from tf_keras import backend
 from tf_keras.saving import register_keras_serializable
 from tf_keras.src.losses import LossFunctionWrapper
@@ -11,12 +8,14 @@ from tf_keras.src.utils import losses_utils, tf_utils
 @register_keras_serializable(package='SegMe>Loss')
 class WeightedLossFunctionWrapper(LossFunctionWrapper):
     def call(self, y_true, y_pred, sample_weight=None):
-        if tensor_util.is_tensor(y_pred) and tensor_util.is_tensor(y_true):
+        if tf.is_tensor(y_pred) and tf.is_tensor(y_true):
             if sample_weight is None:
                 y_pred, y_true = losses_utils.squeeze_or_expand_dimensions(y_pred, y_true)
             else:
                 y_pred, y_true, sample_weight = losses_utils.squeeze_or_expand_dimensions(y_pred, y_true, sample_weight)
-        ag_fn = autograph.tf_convert(self.fn, ag_ctx.control_status_ctx())
+
+        ag_fn = tf.__internal__.autograph.tf_convert(
+            self.fn, tf.__internal__.autograph.control_status_ctx())
 
         return ag_fn(y_true, y_pred, sample_weight, **self._fn_kwargs)
 
@@ -24,10 +23,11 @@ class WeightedLossFunctionWrapper(LossFunctionWrapper):
         # If we are wrapping a lambda function strip '<>' from the name as it is not accepted in scope name.
         graph_ctx = tf_utils.graph_context_for_symbolic_tensors(y_true, y_pred, sample_weight)
         with backend.name_scope(self._name_scope), graph_ctx:
-            if context.executing_eagerly():
+            if tf.executing_eagerly():
                 call_fn = self.call
             else:
-                call_fn = autograph.tf_convert(self.call, ag_ctx.control_status_ctx())
+                call_fn = tf.__internal__.autograph.tf_convert(
+                    self.call, tf.__internal__.autograph.control_status_ctx())
 
             # Rescaling disabled, should be done in loss function with weighted_loss(...) call.
             # pred_mask = losses_utils.get_mask(y_pred)
